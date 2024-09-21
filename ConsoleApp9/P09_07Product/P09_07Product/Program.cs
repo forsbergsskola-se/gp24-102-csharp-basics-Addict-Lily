7d24e8698f5d6b8976d87fb0af6316e2b5de841b
Svetlana Stepanovic Saine <svetlana.stepanovic.saine@student.forsbergsskola.se>
2024-09-21 14:05:54 +0200

    Product

---
+++ ConsoleApp9/P09_07Product/P09_07Product/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Write two numbers");
+
+int input1 = int.Parse(Console.ReadLine());
+int input2 = int.Parse(Console.ReadLine());
+
+int multiplication = input1 * input2;
+
+Console.WriteLine(multiplication);
