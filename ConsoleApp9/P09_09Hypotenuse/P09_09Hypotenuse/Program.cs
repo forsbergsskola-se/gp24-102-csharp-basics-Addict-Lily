65fe6b3c0a67e270ba6348f523e3eb2c14fc2be3
Svetlana Stepanovic Saine <svetlana.stepanovic.saine@student.forsbergsskola.se>
2024-09-21 15:59:56 +0200

    Hypotenuse

---
+++ ConsoleApp9/P09_09Hypotenuse/P09_09Hypotenuse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Write side a and side b of the right triangle:");
+
+float a = float.Parse(Console.ReadLine());
+float b = float.Parse(Console.ReadLine());
+
+float hypotenuse = a * a + b * b;
+
+Console.WriteLine($"The length of the hypotenuse of a right triangle is {Math.Sqrt(hypotenuse)}.");
+
+
+
