40f2b81e48feebd0a8e285b261fe2ecec956089c
Svetlana Stepanovic Saine <svetlana.stepanovic.saine@student.forsbergsskola.se>
2024-09-24 14:10:28 +0200

    changes

--- P10Boolean/P10Boolean/Program.cs
+++ P10Boolean/P10Boolean/Program.cs
@@ -4,13 +4,11 @@ Console.WriteLine("How old are you?");
 string userInput = Console.ReadLine();
 int age = int.Parse(userInput);
 
-bool isChild = age >= 0 && age <= 12;
+bool isChild = age <= 12;
 Console.WriteLine($"You are a child: {isChild}");
 
 bool isTeenager = age >= 13 && age < 19;
 Console.WriteLine($"You are a teenager: {isTeenager}");
 
-bool isAdult = age > 13 && age >= 19;
+bool isAdult = age >= 19;
 Console.WriteLine($"You are an adult: {isAdult}");
-
-Console.ReadLine();
