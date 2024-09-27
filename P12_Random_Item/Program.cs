04bcdbbb047a3382f6b555c6eca86b7dd4be1fd9
Svetlana Stepanovic Saine <svetlana.stepanovic.saine@student.forsbergsskola.se>
2024-09-27 14:35:23 +0200

    Done

---
+++ P12_Random_Item/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Press enter for your new item");
+
+Console.ReadLine();
+
+string[] items = {"Bow and Arrow", "Magic Wand", "Axe", "Sword", "Magic Potion"};
+
+Random random = new Random();
+
+int randomNumber = random.Next(items.Length);
+string randomItem = items[randomNumber];
+
+Console.WriteLine($"Your item is: {randomItem}");
+
+
