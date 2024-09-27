0fefa6ec3a4ee1f62228625eac8575a7b6ada451
Svetlana Stepanovic Saine <svetlana.stepanovic.saine@student.forsbergsskola.se>
2024-09-27 13:03:52 +0200

    Done

---
+++ P12_2Random_Password/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+Console.WriteLine("Random password with max 6 characters:");
+
+string chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz$%#@!*^&\";";
+
+Random random = new Random();
+
+string password = String.Empty;
+
+for (int i = 0; i < 6; i++)
+{
+    password += chars[random.Next(chars.Length)];
+}
+
+Console.WriteLine($"Your new password is: {password}.");
+
+
+string gotoPassword = string.Empty;
+
+backtrack:
+
+gotoPassword += chars[random.Next(chars.Length)];
+
+if (gotoPassword.Length < 6)
+{
+    goto backtrack;
+}
+Console.WriteLine($"Your other new password is: {gotoPassword}.");
