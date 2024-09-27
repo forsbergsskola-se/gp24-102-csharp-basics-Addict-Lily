885bdb5a4aba34880578cb1a030d4bf341013f2e
Svetlana Stepanovic Saine <svetlana.stepanovic.saine@student.forsbergsskola.se>
2024-09-27 11:06:40 +0200

    Done

---
+++ P12_1Random_Coordinates/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Give me the x, y coordinates of my enemy.");
+
+Random random = new Random();
+int xCoordinate = random.Next(1, 100);
+int yCoordinate = random.Next(1, 100);
+
+Console.WriteLine($"Your enemy is at position {xCoordinate}, {yCoordinate}");
