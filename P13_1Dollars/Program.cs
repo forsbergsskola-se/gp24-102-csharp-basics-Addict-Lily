cd631be2e4545b31f234d46b7547ed9a64276d6e
Svetlana Stepanovic Saine <svetlana.stepanovic.saine@student.forsbergsskola.se>
2024-09-29 07:13:03 +0200

    Done by Fredrik

---
+++ P13_1Dollars/Program.cs
@@ -0,0 +1,13 @@
+﻿//E.g. by Fredrik
+
+Console.WriteLine("Welcome to the bank of Cyberica!");
+Console.WriteLine("Please input how much you want to withdraw:");
+
+int withDrawAmount;
+
+while (!int.TryParse(Console.ReadLine(), out withDrawAmount))
+{
+    Console.WriteLine("Please, try again");
+}
+Console.WriteLine("Thank you");
+Console.WriteLine($"Here is your desired amount: {new string('$', withDrawAmount)}");
