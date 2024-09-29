0a9f4f02f42c73e681ae9cf6cffff43bdfca056b
Svetlana Stepanovic Saine <svetlana.stepanovic.saine@student.forsbergsskola.se>
2024-09-29 07:34:20 +0200

    Done by Fredrik

---
+++ P13_2Triangle/Program.cs
@@ -0,0 +1,18 @@
+﻿//E.g. by Fredrik
+
+Console.WriteLine("Please input the desired size of your triangle:");
+
+retry:
+bool parsed = int.TryParse(Console.ReadLine(), out int desiredSize);
+
+if (!parsed)
+{
+    Console.WriteLine("Please try only numerical values:");
+    goto retry;
+}
+
+continuation:
+Console.WriteLine(new string('#', desiredSize--));
+
+if(desiredSize > 0)
+    goto continuation;
